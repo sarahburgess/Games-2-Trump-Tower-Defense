d7ba273a7a619fb8532da2d5f5e280ec76c577ab
sarahburgess <burgesssa1@gcc.edu>
2016-02-14 18:15:24 -0500

    b
    b

--- d3dUtil.h
+++ d3dUtil.h
@@ -138,12 +138,11 @@ const D3DXCOLOR BLUE(0.0f, 0.0f, 1.0f, 1.0f);
 const D3DXCOLOR YELLOW(1.0f, 1.0f, 0.0f, 1.0f);
 const D3DXCOLOR CYAN(0.0f, 1.0f, 1.0f, 1.0f);
 const D3DXCOLOR MAGENTA(1.0f, 0.0f, 1.0f, 1.0f);
-<<<<<<< HEAD
+
 const D3DXCOLOR SUITBLUE(0.1f, 0.1f, 0.48f, 1.0f);
 const D3DXCOLOR GREY(0.8f, 0.79f, 0.79f, 1.0f);
-=======
+
 const D3DXCOLOR CHARCOAL_GREY(54.0f/255.0f, 69.0f/255.0f, 79.0f/255.0f, 1.0f);
->>>>>>> origin/master
 
 const D3DXCOLOR BEACH_SAND(1.0f, 0.96f, 0.62f, 1.0f);
 const D3DXCOLOR LIGHT_YELLOW_GREEN(0.48f, 0.77f, 0.46f, 1.0f);
